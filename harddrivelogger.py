f37750e265f265980e73299828cacca09ad698ee
ArtemZinets <zinetsartem@gmail.com>
2025-09-14 12:38:53 -0300

    Kind of finished this thing?

--- harddrivelogger.py
+++ harddrivelogger.py
@@ -8,6 +8,7 @@
 import subprocess
 import multiprocessing
 import os
+from playground import add_to_log as log
 
 
 def start_monitoring():
@@ -23,9 +24,9 @@ def stop_monitoring():
     print("stop_monitoring call")
     result : subprocess.CompletedProcess = subprocess.run("sudo auditctl -d always,exit -F dir=/home -F perm=rwx".split(" "),capture_output=True,text=True)
     if result.returncode == 0:
-        print("Starting auditing /home...")
+        print("Stopping auditing /home...")
     else:
-        print("Error occured when tried to audit /home: ",result.stderr)
+        print("Error occured when tried to stop auditting /home: ",result.stderr)
 
 
 def audit_function():
@@ -35,15 +36,11 @@ def audit_function():
     for line in audit_process.stdout: #type: ignore (idk why is this line shows an error because it actually works perfectly fine)
         event_id : str = line.split(" ")[1].split(":")[1][0:-1]
         if current_event != event_id:
-            print()
-            print(" ".join(["sudo","ausearch","--intepret","--event",current_event]))
             current_event = event_id
             audit_intepreter : subprocess.CompletedProcess = subprocess.run(["sudo","ausearch","--interpret","--event",current_event],capture_output=True,text=True)
             if audit_intepreter.returncode == 0:
-                print(audit_intepreter.stdout)
-                
-                
                 transaction : dict = {}
+                cancelled : bool = True
                 for subline in audit_intepreter.stdout.split("\n"):
                     if len(subline) <= 0 or subline[0] == "-":
                         continue
@@ -51,17 +48,35 @@ def audit_function():
                     body : str = subline[subline.find(" : ")+3:]
                     
                     transaction_type : str = header[header.find("type=")+5:header.find(" msg=")]
-                    transaction["type"] = transaction_type
 
                     timestamp : str = header[header.find("msg=audit(")+len("msg=audit("):header.rfind(":")]
                     transaction["timestamp"] = timestamp
 
                     transaction["event_id"] = event_id
-
+                    item_number : int = 0
                     if transaction_type == "PROCTITLE":
-                        transaction["proctitle"] = body[body.find("=")+1:]
-                    if transaction_type == "PATH":
-                        pass 
+                        transaction["executable"] = body[body.find("=")+1:-1]
+                        cancelled = False
+                    elif transaction_type == "PATH":
+                        cancelled = False
+                        if int(body[body.find("item=")+5:body.find("name=")]) < item_number:
+                            continue
+                        item_number = int(body[body.find("item=")+5:body.find("name=")])
+                        transaction["process_accessed_path"] = body[body.find("name=")+5:body.find("inode=")-1]
+                    elif transaction_type == "SYSCALL":
+                        cancelled = False
+                        transaction["syscall"] = body[body.find("syscall=")+8:body.find("success=")-1]
+                        transaction["success"] = body[body.find("success=")+8:body.find("exit=")-1]
+                        transaction["kernel_return"] = body[body.find("exit=")+5:body.find("a0=")-1]
+                        transaction["arguments"] = body[body.find("a0="):body.find("items=")-1]
+                        transaction["user_id"] = body[body.find("uid=")+4:body.find("gid=")-1]
+                        transaction["authed_user_id"] = body[body.find("auid=")+5:body.find("uid=")-1]
+                        transaction["process_id"] = body[body.find(" pid=")+5:body.find("auid=")-1]
+                
+                if not cancelled:
+                    if not (transaction["executable"][:transaction["executable"].find("--event")] == "sudo ausearch --interpret "):
+                        log(transaction)
+                        print()
             else:
                 print("Error! ",audit_intepreter.stderr)
 
@@ -71,7 +86,7 @@ def audit_function():
 start_monitoring()
 auditter = multiprocessing.Process(target=audit_function)
 auditter.start()
-auditter.join(5) # DO NOT CTRL+C THE PROGRAM
+auditter.join(60) # DO NOT CTRL+C THE PROGRAM
 auditter.terminate()
 stop_monitoring()
 
